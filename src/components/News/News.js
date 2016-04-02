fb6a70469316ca8647d3cf4c1e71b3dda55e2799
Sunny Singh <sunny@sunnyis.me>
2016-04-02 11:09:01 -0400

    Add icons

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -2,21 +2,27 @@ import React from 'react';
 import styles from './News.css';
 import Tabs from 'material-ui/lib/tabs/tabs';
 import Tab from 'material-ui/lib/tabs/tab';
+import HackerNewsIcon from '../Icons/HackerNews';
+import GitHubIcon from '../Icons/GitHub';
+import ProductHuntIcon from '../Icons/ProductHunt';
 import HackerNews from '../HackerNews/HackerNews';
 
 const News = (props) => {
     return (
         <Tabs
-            className={styles.container}
+            className={styles.tabsContainer}
             contentContainerClassName={styles.content}
         >
-            <Tab label="Hacker News">
-                <HackerNews />
+            <Tab icon={<HackerNewsIcon title="Hacker News" />}>
+                <h1 className={styles.heading}>
+                    Hacker News
+                </h1>
+                <HackerNews className={styles.storiesContainer} />
             </Tab>
-            <Tab label="GitHub Trending">
+            <Tab icon={<GitHubIcon title="GitHub Trending" />}>
                 GitHub trending repositories here...
             </Tab>
-            <Tab label="Product Hunt">
+            <Tab icon={<ProductHuntIcon title="Product Hunt" />}>
                 Product Hunt tech here...
             </Tab>
         </Tabs>
