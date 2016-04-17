d9bbe8930d323eb3b27a93a39480fcdb792a23d4
Sunny Singh <sunny@sunnyis.me>
2016-04-17 15:51:55 -0400

    Fix issue #2 where PH wouldn't work on Safari

--- src/data/ProductHuntData.js
+++ src/data/ProductHuntData.js
@@ -7,26 +7,50 @@ ProductHuntData.config = (config) => {
 };
 
 ProductHuntData.getItems = (callback) => {
-    const baseUrl = 'https://api.producthunt.com/v1';
+    const baseUrl = 'https://wrapapi.com/use/sunnysingh/producthunt/todaytech/0.0.2?wrapAPIKey=vZpCx0QXD65gAcUD4Q7gAL6y0GQB1pgT';
 
     ProductHuntData.request
-        .get(baseUrl+'/posts')
-        .set('Authorization', 'Bearer bbb38245895a24cf4a2546fa2a96a20fa8bec59badc1a327acd3b5afe449b73f')
+        .get(baseUrl)
         .end((error, response) => {
             let data = [];
-            for (let product of response.body.posts) {
+            for (let product of response.body.data.posts) {
                 data.push({
                     id:            product.id,
                     name:          product.name,
                     tagline:       product.tagline,
-                    url:           product.redirect_url,
-                    votesCount:    product.votes_count,
-                    commentsCount: product.comments_count,
-                    discussionUrl: product.discussion_url,
+                    url:           'https://www.producthunt.com'+product.shortened_url,
+                    votesCount:    product.vote_count,
+                    commentsCount: product.comment_count,
+                    discussionUrl: 'https://www.producthunt.com'+product.url,
                 });
             }
             callback(data);
         });
 };
+
+// Directly using Product Hunt API. Issues on Safari.
+//
+// ProductHuntData.getItems = (callback) => {
+//     const baseUrl = 'https://api.producthunt.com/v1';
+//
+//     ProductHuntData.request
+//         .get(baseUrl+'/posts')
+//         .set('Authorization', 'Bearer bbb38245895a24cf4a2546fa2a96a20fa8bec59badc1a327acd3b5afe449b73f')
+//         .end((error, response) => {
+//             let data = [];
+//             for (let product of response.body.posts) {
+//                 data.push({
+//                     id:            product.id,
+//                     name:          product.name,
+//                     tagline:       product.tagline,
+//                     url:           product.redirect_url,
+//                     votesCount:    product.votes_count,
+//                     commentsCount: product.comments_count,
+//                     discussionUrl: product.discussion_url,
+//                 });
+//             }
+//             callback(data);
+//         });
+// };
 
 export default ProductHuntData;
