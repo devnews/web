1086b7431ec647b9033de500a8d694d676ec869f
Sunny Singh <me@sunnysingh.io>
2017-11-26 12:31:01 -0500

    Remove codesponsor.io badge

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Theme from '../../../config/theme';
-import FloatingActionButton from 'material-ui/FloatingActionButton';
-import UpIcon from 'material-ui/svg-icons/navigation/arrow-upward';
 import Header from '../Header';
 import News from '../News';
-import Sponsor from '../Sponsor';
 import styles from './index.css';
 
 class App extends React.Component {
@@ -16,7 +13,6 @@ class App extends React.Component {
                 <div>
                     <Header />
                     <News />
-                    <Sponsor />
                 </div>
             </MuiThemeProvider>
         )
